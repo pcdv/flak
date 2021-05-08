ce3863aaaea2fc1152caaaca6c4da0e1748d77b6
pcdv <pcdv.github@gmail.com>
2021-05-08 20:56:50 +0200

    New annotation @WithAnyPermission allows to check several user permissions

--- flak-login/src/main/java/flak/login/CheckPermission.java
+++ flak-login/src/main/java/flak/login/CheckPermission.java
@@ -1,6 +1,8 @@
 package flak.login;
 
 import java.net.HttpURLConnection;
+import java.util.Collections;
+import java.util.HashSet;
 
 import flak.HttpException;
 import flak.spi.AbstractMethodHandler;
@@ -14,7 +16,7 @@ public class CheckPermission implements BeforeHook {
 
   private final SessionManager manager;
 
-  private final String permission;
+  private final HashSet<String> permissions = new HashSet<>();
 
   public CheckPermission(AbstractMethodHandler handler,
                          SessionManager manager) {
@@ -23,18 +25,31 @@ public class CheckPermission implements BeforeHook {
     WithPermission perm =
       handler.getJavaMethod().getAnnotation(WithPermission.class);
 
-    this.permission = perm == null ? null : perm.value();
+    if (perm != null)
+      permissions.add(perm.value());
 
+    WithAnyPermission perms =
+      handler.getJavaMethod().getAnnotation(WithAnyPermission.class);
+
+    if (perms != null)
+      Collections.addAll(permissions, perms.value());
   }
 
   @Override
   public void execute(SPRequest request) {
-    if (permission != null) {
+    if (!permissions.isEmpty()) {
       FlakSession session = manager.getCurrentSession(request);
-      if (session == null || !session.getUser().hasPermission(permission)) {
-        throw new HttpException(HttpURLConnection.HTTP_UNAUTHORIZED,
-                                "No permission");
+      if (session == null || !hasPermission(session.getUser())) {
+        throw new HttpException(HttpURLConnection.HTTP_UNAUTHORIZED, "No permission");
       }
     }
+  }
+
+  private boolean hasPermission(FlakUser user) {
+    for (String p : permissions) {
+      if (user.hasPermission(p))
+        return true;
+    }
+    return false;
   }
 }
--- flak-tests/src/test/java/flask/test/PermissionTest.java
+++ flak-tests/src/test/java/flask/test/PermissionTest.java
@@ -7,6 +7,7 @@ import flak.annotations.Post;
 import flak.annotations.Route;
 import flak.login.DefaultUser;
 import flak.login.FlakUser;
+import flak.login.WithAnyPermission;
 import flak.login.WithPermission;
 import org.junit.Assert;
 import org.junit.Test;
@@ -20,17 +21,23 @@ public class PermissionTest extends AbstractAppTest {
     initFlakLogin();
 
     FlakUser foo = sessionManager.createUser("foo");
+    sessionManager.addUser(foo);
+
     FlakUser joe = sessionManager.createUser("joe");
     ((DefaultUser) joe).addPermission("access");
-    sessionManager.addUser(foo);
     sessionManager.addUser(joe);
+
+    FlakUser root = sessionManager.createUser("admin");
+    ((DefaultUser) root).addPermission("admin");
+    sessionManager.addUser(root);
   }
 
   @Route("/api/login")
   @Post
   public void login(Form form) {
     FlakUser user = sessionManager.getUser(form.get("login"));
-    sessionManager.openSession(app, user, app.getResponse());
+    if (user != null)
+      sessionManager.openSession(app, user, app.getResponse());
   }
 
   @WithPermission("access")
@@ -39,17 +46,36 @@ public class PermissionTest extends AbstractAppTest {
     return "OK";
   }
 
+  @WithAnyPermission({"access", "admin"})
+  @Route("/api/data2")
+  public String getData2() {
+    return "OK";
+  }
+
   @Test
   public void testPermission() throws IOException {
     // not logged in
     TestUtil.assertFails(() -> client.get("/api/data"), "No permission");
+    TestUtil.assertFails(() -> client.get("/api/data2"), "No permission");
+
+    // unknown user
+    client.post("/api/login", "login=unknown");
+    TestUtil.assertFails(() -> client.get("/api/data"), "No permission");
+    TestUtil.assertFails(() -> client.get("/api/data2"), "No permission");
 
     // logged in as user with no permission
     client.post("/api/login", "login=foo");
     TestUtil.assertFails(() -> client.get("/api/data"), "No permission");
+    TestUtil.assertFails(() -> client.get("/api/data2"), "No permission");
 
     // logged in as user with required permission
     client.post("/api/login", "login=joe");
     Assert.assertEquals("OK", client.get("/api/data"));
+    Assert.assertEquals("OK", client.get("/api/data2"));
+
+    // logged in as user with other required permission
+    client.post("/api/login", "login=admin");
+    TestUtil.assertFails(() -> client.get("/api/data"), "No permission");
+    Assert.assertEquals("OK", client.get("/api/data2"));
   }
 }
